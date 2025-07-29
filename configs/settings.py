39555004fa4c57b22f369b5376660639a892ba71
Hyerin Sung <hyerinx@ewha.ac.kr>
2025-07-29 16:33:58 +0900

    🔧 media 설정 (#1)

--- configs/settings.py
+++ configs/settings.py
@@ -1,3 +1,4 @@
+import os
 from env import BASE_DIR, DEBUG, SECRET_KEY, DATABASE_DEFAULT
 
 DEBUG = DEBUG
@@ -90,6 +91,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+# Media files
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Default primary key field type
