60b98b7f88d021aac9c5867fdd92b75e630b7ecf
Hyerin Sung <hyerinx@ewha.ac.kr>
2025-07-30 21:24:07 +0900

    💡 docstring 추가 (#6)

--- utils/decorators.py
+++ utils/decorators.py
@@ -11,6 +11,7 @@ def example(func):
     return wrapper
 
 def validate_data(func):
+    """클라이언트가 전송한 데이터가 유효한지 검증합니다."""
     @wraps(func)
     def wrapper(self, *args, **kwargs):
         if not self.serializer.is_valid():
@@ -22,6 +23,7 @@ def validate_data(func):
     return wrapper
 
 def validate_permission(func):
+    """클라이언트가 해당 인스턴스에 대하여 요청을 수행할 권한을 갖고 있는지 검증합니다."""
     @wraps(func)
     def wrapper(self, *args, **kwargs):
         if self.instance.user != self.request.user:
@@ -33,6 +35,7 @@ def validate_permission(func):
     return wrapper
 
 def validate_unique(func):
+    """클라이언트의 요청이 UNIQUE 제약조건을 준수하는지 검증합니다."""
     @wraps(func)
     def wrapper(self, *args, **kwargs):
         try:
