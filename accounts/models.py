a61e5bec0dda4a62d67d61b8090915d885a3ace5
Hyerin Sung <hyerinx@ewha.ac.kr>
2025-08-02 15:50:46 +0900

    🔨 CustomUser 모델 추가 (#2)

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,28 @@
+from string import ascii_lowercase, digits
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django_nanoid.models import NANOIDField
+from .managers import CustomUserManager
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    # AbstractUser 모델 오버라이딩
+    username = NANOIDField(
+        editable=False,
+        secure_generated=True,
+        alphabetically=ascii_lowercase+digits,
+        size=21,
+    )
+    first_name = None
+    last_name = None
+    email = models.EmailField(
+        unique=True,
+        error_messages={'unique': '이미 존재하는 이메일입니다.',},
+    )
+    objects = CustomUserManager()
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    # 커스텀 필드
+
+    def __str__(self):
+        return self.email
--- configs/settings/base.py
+++ configs/settings/base.py
@@ -61,7 +61,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'configs.wsgi.application'
 
-AUTH_USER_MODEL = 'auth.User'
+AUTH_USER_MODEL = 'accounts.CustomUser'
 
 
 # Password validation
