d8c655da476ba4a52a327b9c6d1acd3751134818
2ewyeonwoo3 <yeonwooo10607@ewha.ac.kr>
2025-08-12 00:37:52 +0900

    세부 업종(industry) 추가

--- utils/choices.py
+++ utils/choices.py
@@ -9,8 +9,29 @@ class ExampleChoices(IntegerChoices):
     THREE = 3, '삼'
 
 class IndustryChoices(TextChoices):          # Founder, Proposer, Funding, Proposal 모델에 사용
-    FOOD        = "food",        "음식점"
-    CAFE        = "cafe",        "카페/디저트"
+    FOOD_DINING          = "food_dining",          "외식/음식점"          
+    CAFE_DESSERT         = "cafe_dessert",         "카페/디저트"          
+    PUB_BAR              = "pub_bar",              "주점"               
+    CONVENIENCE_RETAIL   = "convenience_retail",   "편의점/소매"          
+    GROCERY_MART         = "grocery_mart",         "마트/식료품"         
+    BEAUTY_CARE          = "beauty_care",          "뷰티/미용"             
+    HEALTH_FITNESS       = "health_fitness",       "건강"               
+    FASHION_GOODS        = "fashion_goods",        "패션/잡화"              
+    HOME_LIVING_INTERIOR = "home_living_interior", "생활용품/가구/인테리어"  
+    HOBBY_LEISURE        = "hobby_leisure",        "취미/오락/여가"         
+    CULTURE_BOOKS        = "culture_books",        "문화/서적"           
+    PET                  = "pet",                  "반려동물"        
+    LODGING              = "lodging",              "숙박"          
+    EDUCATION_ACADEMY    = "education_academy",    "교육/학원"            
+    AUTO_TRANSPORT       = "auto_transport",       "자동/운송"            
+    IT_OFFICE            = "it_office",            "IT/사무"             
+    FINANCE_LEGAL_TAX    = "finance_legal_tax",    "금융/법률/회계"         
+    MEDICAL_PHARMA       = "medical_pharma",       "의료/의약"           
+    PERSONAL_SERVICES    = "personal_services",    "생활 서비스"       
+    FUNERAL_WEDDING      = "funeral_wedding",      "장례/예식"             
+    PHOTO_STUDIO         = "photo_studio",         "사진/스튜디오"       
+    OTHER_RETAIL         = "other_retail",         "기타 판매업"
+    OTHER_SERVICE        = "other_service",        "기타 서비스업"
 
 class FounderTargetChoices(TextChoices):
     LOCAL = "local", "동네주민"
