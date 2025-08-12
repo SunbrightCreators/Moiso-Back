a816fc860dc3ed5fb4f1a6321f0904ad6c27eb70
2ewyeonwoo3 <yeonwooo10607@ewha.ac.kr>
2025-08-13 07:43:09 +0900

    FundingBankCategoryChoices 추가

--- utils/choices.py
+++ utils/choices.py
@@ -9,33 +9,34 @@ class ExampleChoices(IntegerChoices):
     THREE = 3, '삼'
 
 class IndustryChoices(TextChoices):          # Founder, Proposer, Funding, Proposal 모델에 사용
-    FOOD_DINING          = "food_dining",          "외식/음식점"          
-    CAFE_DESSERT         = "cafe_dessert",         "카페/디저트"          
-    PUB_BAR              = "pub_bar",              "주점"               
-    CONVENIENCE_RETAIL   = "convenience_retail",   "편의점/소매"          
-    GROCERY_MART         = "grocery_mart",         "마트/식료품"         
-    BEAUTY_CARE          = "beauty_care",          "뷰티/미용"             
-    HEALTH_FITNESS       = "health_fitness",       "건강"               
-    FASHION_GOODS        = "fashion_goods",        "패션/잡화"              
-    HOME_LIVING_INTERIOR = "home_living_interior", "생활용품/가구/인테리어"  
-    HOBBY_LEISURE        = "hobby_leisure",        "취미/오락/여가"         
-    CULTURE_BOOKS        = "culture_books",        "문화/서적"           
-    PET                  = "pet",                  "반려동물"        
-    LODGING              = "lodging",              "숙박"          
-    EDUCATION_ACADEMY    = "education_academy",    "교육/학원"            
-    AUTO_TRANSPORT       = "auto_transport",       "자동/운송"            
-    IT_OFFICE            = "it_office",            "IT/사무"             
-    FINANCE_LEGAL_TAX    = "finance_legal_tax",    "금융/법률/회계"         
-    MEDICAL_PHARMA       = "medical_pharma",       "의료/의약"           
-    PERSONAL_SERVICES    = "personal_services",    "생활 서비스"       
-    FUNERAL_WEDDING      = "funeral_wedding",      "장례/예식"             
-    PHOTO_STUDIO         = "photo_studio",         "사진/스튜디오"       
-    OTHER_RETAIL         = "other_retail",         "기타 판매업"
-    OTHER_SERVICE        = "other_service",        "기타 서비스업"
+    FOOD_DINING          = "FOOD_DINING",          "외식/음식점"
+    CAFE_DESSERT         = "CAFE_DESSERT",         "카페/디저트"
+    PUB_BAR              = "PUB_BAR",              "주점"
+    CONVENIENCE_RETAIL   = "CONVENIENCE_RETAIL",   "편의점/소매"
+    GROCERY_MART         = "GROCERY_MART",         "마트/식료품"
+    BEAUTY_CARE          = "BEAUTY_CARE",          "뷰티/미용"
+    HEALTH_FITNESS       = "HEALTH_FITNESS",       "건강"
+    FASHION_GOODS        = "FASHION_GOODS",        "패션/잡화"
+    HOME_LIVING_INTERIOR = "HOME_LIVING_INTERIOR", "생활용품/가구/인테리어"
+    HOBBY_LEISURE        = "HOBBY_LEISURE",        "취미/오락/여가"
+    CULTURE_BOOKS        = "CULTURE_BOOKS",        "문화/서적"
+    PET                  = "PET",                  "반려동물"
+    LODGING              = "LODGING",              "숙박"
+    EDUCATION_ACADEMY    = "EDUCATION_ACADEMY",    "교육/학원"
+    AUTO_TRANSPORT       = "AUTO_TRANSPORT",       "자동/운송"
+    IT_OFFICE            = "IT_OFFICE",            "IT/사무"
+    FINANCE_LEGAL_TAX    = "FINANCE_LEGAL_TAX",    "금융/법률/회계"
+    MEDICAL_PHARMA       = "MEDICAL_PHARMA",       "의료/의약"
+    PERSONAL_SERVICES    = "PERSONAL_SERVICES",    "생활 서비스"
+    FUNERAL_WEDDING      = "FUNERAL_WEDDING",      "장례/예식"
+    PHOTO_STUDIO         = "PHOTO_STUDIO",         "사진/스튜디오"
+    OTHER_RETAIL         = "OTHER_RETAIL",         "기타 판매업"
+    OTHER_SERVICE        = "OTHER_SERVICE",        "기타 서비스업"
+
 
 class FounderTargetChoices(TextChoices):
-    LOCAL = "local", "동네주민"
-    OUTSIDER = "outsider", "외부인"
+    LOCAL = "LOCAL", "동네주민"
+    STRANGER = "STRANGER", "외부인"
 
 class FundingStatusChoices(TextChoices):
     REVIEW = "REVIEW", "심사중"
@@ -44,6 +45,9 @@ class FundingStatusChoices(TextChoices):
     ACTIVE = "ACTIVE", "진행중"
     CLOSED = "CLOSED", "종료"
 
+class FundingBankCategoryChoices(TextChoices):
+    INDIVIDUAL = "INDIVIDUAL", "개인"
+    BUSINESS = "BUSINESS", "사업자"
 
 class RewardCategoryChoices(TextChoices):
     COUPON = "coupon", "펀딩 할인쿠폰"
@@ -62,14 +66,14 @@ class PaymentTypeChoices(TextChoices):
     BRANDPAY = "BRANDPAY", "브랜드페이"
 
 class PaymentMethodChoices(TextChoices):
-    CARD = "CARD", "카드"
-    VIRTUAL_ACCOUNT = "VIRTUAL_ACCOUNT", "가상계좌"
-    EASY_PAY = "EASY_PAY", "간편결제"
-    MOBILE_PHONE = "MOBILE_PHONE", "휴대폰"
-    TRANSFER = "TRANSFER", "계좌이체"
-    GIFT_CERTIFICATE = "GIFT_CERTIFICATE", "문화상품권"
-    CULTURE_GIFT_CERT = "CULTURE_GIFT_CERT", "도서문화상품권"
-    GAME_GIFT_CERT = "GAME_GIFT_CERT", "게임문화상품권"
+    CARD = "카드"
+    VIRTUAL_ACCOUNT = "가상계좌"
+    EASY_PAY = "간편결제"
+    MOBILE_PHONE = "휴대폰"
+    TRANSFER = "계좌이체"
+    GIFT_CERTIFICATE = "문화상품권"
+    CULTURE_GIFT_CERT = "도서문화상품권"
+    GAME_GIFT_CERT = "게임문화상품권"
 
 class PaymentStatusChoices(TextChoices):
     READY = "READY", "요청 생성"
