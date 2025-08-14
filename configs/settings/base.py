3878e7b5c1257d5ad609fe02ea13f3734411498c
Hyerin Sung <hyerinx@ewha.ac.kr>
2025-08-15 00:12:42 +0900

    🔨 maps 앱 추가 (#26)

--- configs/settings/base.py
+++ configs/settings/base.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'accounts.apps.AccountsConfig',
+    'maps.apps.MapsConfig',
 ]
 
 MIDDLEWARE = [
--- configs/urls.py
+++ configs/urls.py
@@ -6,4 +6,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
+    path('maps/', include('maps.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
