c73abea5c1624ccad708a72f54ea48105f49ee62
Hyerin Sung <hyerinx@ewha.ac.kr>
2025-08-18 18:38:44 +0900

    🔨 Notification 추상 모델 추가 (#37)

--- notifications/models.py
+++ notifications/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from utils.choices import NotificationCategoryChoices
 
-# Create your models here.
+class Notification(models.Model):
+    body = models.CharField(
+        max_length=65,
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+    category = models.CharField(
+        max_length=7,
+        choices=NotificationCategoryChoices.choices,
+    )
+    path_variable = models.CharField(
+        max_length=21,
+    )
+
+    class Meta:
+        abstract = True
