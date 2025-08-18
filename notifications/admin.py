fa48a29b6cbb0c27319e219a8b213bda5c69db83
Hyerin Sung <hyerinx@ewha.ac.kr>
2025-08-18 18:40:41 +0900

    🔨 admins.py 모델 등록 (#37)

--- notifications/admin.py
+++ notifications/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import ProposerNotification, FounderNotification
 
-# Register your models here.
+admin.site.register(ProposerNotification)
+admin.site.register(FounderNotification)
