a85f171e305b84c43bf6773939d98a9c42d2b8b4
Hyerin Sung <hyerinx@ewha.ac.kr>
2025-08-18 16:58:48 +0900

    🔨 ProposerScrapProposal 모델 추가 (#36)

--- proposals/models.py
+++ proposals/models.py
@@ -101,3 +101,30 @@ class ProposerLikeProposal(models.Model):
                 violation_error_message='제안자는 제안글을 한 번만 좋아요할 수 있어요.',
             )
         ]
+
+class ProposerScrapProposal(models.Model):
+    user = models.ForeignKey(
+        'accounts.Proposer',
+        on_delete=models.CASCADE,
+        related_name='proposer_scrap_proposal',
+    )
+    proposal = models.ForeignKey(
+        'Proposal',
+        on_delete=models.CASCADE,
+        related_name='proposer_scrap_proposal',
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    def __str__(self):
+        return f'{self.user.user.email} 님이 {self.proposal.title} 제안글을 스크랩했어요.'
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user','proposal'],
+                name='unique_user_proposal',
+                violation_error_message='제안자는 제안글을 한 번만 스크랩할 수 있어요.',
+            )
+        ]
