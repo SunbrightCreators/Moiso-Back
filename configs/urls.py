804f9d89f546df81aeaade4dd836dd5356b35a5f
Hyerin Sung <hyerinx@ewha.ac.kr>
2025-08-18 12:48:16 +0900

    🔨 proposals 앱 추가 (#36)

--- configs/urls.py
+++ configs/urls.py
@@ -6,5 +6,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
+    path('proposals/', include('proposals.urls')),
     path('pays/', include('pays.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
