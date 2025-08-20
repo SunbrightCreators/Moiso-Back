d6426610c740fed6f8a90a732142279c60b9af3f
Hyerin Sung <hyerinx@ewha.ac.kr>
2025-08-21 01:30:08 +0900

    🔨 url 추가 (#53)

--- proposals/urls.py
+++ proposals/urls.py
@@ -4,4 +4,6 @@ from .views import *
 app_name = 'proposals'
 
 urlpatterns = [
+    path('proposer/like', ProposerLike.as_view()),
+    path('<str:profile>/scrap', ProfileScrap.as_view()),
 ]
--- proposals/views.py
+++ proposals/views.py
@@ -2,3 +2,19 @@ from django.http import HttpRequest
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+class ProposerLike(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request:HttpRequest, format=None):
+        pass
+
+class ProfileScrap(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request:HttpRequest, profile, format=None):
+        pass
+
+    def get(self, request:HttpRequest, profile, format=None):
+        address = request.query_params.get('address')
