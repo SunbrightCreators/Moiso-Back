fa4c51fce1b10a96d50d07edb5b725311009a1a0
Hyerin Sung <hyerinx@ewha.ac.kr>
2025-08-21 18:21:04 +0900

    ✨ 제안 스크랩 추가/삭제 API 추가 (#53)

--- proposals/services.py
+++ proposals/services.py
@@ -1,6 +1,6 @@
 from django.http import HttpRequest
 from utils.decorators import validate_data, validate_permission, validate_unique
-from .models import ProposerLikeProposal
+from .models import ProposerLikeProposal, ProposerScrapProposal, FounderScrapProposal
 
 class ProposerLikeProposalService:
     def __init__(self, request:HttpRequest):
@@ -16,6 +16,52 @@ class ProposerLikeProposalService:
                 - `False`: 좋아요 삭제
         '''
         obj, created = ProposerLikeProposal.objects.get_or_create(
+            user__user=self.request.user,
+            proposal=proposal_id,
+        )
+        if created:
+            return True
+        else:
+            obj.delete()
+            return False
+
+class ProposerScrapProposalService:
+    def __init__(self, request:HttpRequest):
+        self.request = request
+
+    def post(self, proposal_id:int) -> bool:
+        '''
+        Args:
+            proposal_id (int): 제안 id
+        Returns:
+            is_created (bool):
+                - `True`: 스크랩 추가
+                - `False`: 스크랩 삭제
+        '''
+        obj, created = ProposerScrapProposal.objects.get_or_create(
+            user__user=self.request.user,
+            proposal=proposal_id,
+        )
+        if created:
+            return True
+        else:
+            obj.delete()
+            return False
+
+class FounderScrapProposalService:
+    def __init__(self, request:HttpRequest):
+        self.request = request
+
+    def post(self, proposal_id:int) -> bool:
+        '''
+        Args:
+            proposal_id (int): 제안 id
+        Returns:
+            is_created (bool):
+                - `True`: 스크랩 추가
+                - `False`: 스크랩 삭제
+        '''
+        obj, created = FounderScrapProposal.objects.get_or_create(
             user__user=self.request.user,
             proposal=proposal_id,
         )
--- proposals/views.py
+++ proposals/views.py
@@ -1,10 +1,13 @@
+from typing import Literal
 from django.http import HttpRequest
+from django.utils.decorators import method_decorator
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from utils.decorators import validate_path_choices
 from .serializers import ProposalIdSerializer
-from .services import ProposerLikeProposalService
+from .services import ProposerLikeProposalService, ProposerScrapProposalService, FounderScrapProposalService
 
 class ProposerLike(APIView):
     permission_classes = [IsAuthenticated]
@@ -32,11 +35,35 @@ class ProposerLike(APIView):
                 status=status.HTTP_200_OK,
             )
 
+@method_decorator(validate_path_choices(profile=('proposer', 'founder')), name='dispatch')
 class ProfileScrap(APIView):
     permission_classes = [IsAuthenticated]
 
-    def post(self, request:HttpRequest, profile, format=None):
-        pass
+    def post(self, request:HttpRequest, profile:Literal['proposer','founder'], format=None):
+        serializer = ProposalIdSerializer(data=request.data)
+        if not serializer.is_valid():
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        proposal_id = serializer.validated_data['proposal_id']
+
+        if profile == 'proposer':
+            service = ProposerScrapProposalService(request)
+        elif profile == 'founder':
+            service = FounderScrapProposalService(request)
+        is_created = service.post(proposal_id)
+
+        if is_created:
+            return Response(
+                { 'detail': '이 제안글을 스크랩했어요.' },
+                status=status.HTTP_201_CREATED,
+            )
+        else:
+            return Response(
+                { 'detail': '스크랩을 취소했어요.' },
+                status=status.HTTP_200_OK,
+            )
 
     def get(self, request:HttpRequest, profile, format=None):
         address = request.query_params.get('address')
