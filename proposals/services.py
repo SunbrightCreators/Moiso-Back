6001cbd1694afba105de1990b2cce09bc4918d7a
Hyerin Sung <hyerinx@ewha.ac.kr>
2025-08-21 18:16:35 +0900

    ✨ 제안 좋아요 추가/삭제 API 추가 (#53)

--- proposals/services.py
+++ proposals/services.py
@@ -1,2 +1,26 @@
 from django.http import HttpRequest
 from utils.decorators import validate_data, validate_permission, validate_unique
+from .models import ProposerLikeProposal
+
+class ProposerLikeProposalService:
+    def __init__(self, request:HttpRequest):
+        self.request = request
+
+    def post(self, proposal_id:int) -> bool:
+        '''
+        Args:
+            proposal_id (int): 제안 id
+        Returns:
+            is_created (bool):
+                - `True`: 좋아요 추가
+                - `False`: 좋아요 삭제
+        '''
+        obj, created = ProposerLikeProposal.objects.get_or_create(
+            user__user=self.request.user,
+            proposal=proposal_id,
+        )
+        if created:
+            return True
+        else:
+            obj.delete()
+            return False
--- proposals/views.py
+++ proposals/views.py
@@ -3,12 +3,34 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from .serializers import ProposalIdSerializer
+from .services import ProposerLikeProposalService
 
 class ProposerLike(APIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request:HttpRequest, format=None):
-        pass
+        serializer = ProposalIdSerializer(data=request.data)
+        if not serializer.is_valid():
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        proposal_id = serializer.validated_data['proposal_id']
+
+        service = ProposerLikeProposalService(request)
+        is_created = service.post(proposal_id)
+
+        if is_created:
+            return Response(
+                { 'detail': '이 제안글을 좋아해요.' },
+                status=status.HTTP_201_CREATED,
+            )
+        else:
+            return Response(
+                { 'detail': '좋아요를 취소했어요.' },
+                status=status.HTTP_200_OK,
+            )
 
 class ProfileScrap(APIView):
     permission_classes = [IsAuthenticated]
