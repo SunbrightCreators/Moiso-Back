35ec81c2430b5c5af0c274df4588c44a1d1c665c
Hyerin Sung <hyerinx@ewha.ac.kr>
2025-08-21 14:20:05 +0900

    🔥 ProposalIdSerializer error_messages 삭제 (#53)

--- proposals/serializers.py
+++ proposals/serializers.py
@@ -7,12 +7,6 @@ class ProposalIdSerializer(serializers.Serializer):
         required=True,
         allow_null=False,
         min_value=1,
-        error_messages={
-            'required': 'proposal_id는 반드시 입력해야 합니다.',
-            'null': 'proposal_id에 null 값을 입력할 수 없습니다.',
-            'invalid': 'proposal_id는 정수여야 합니다.',
-            'min_value': 'proposal_id는 1 이상이어야 합니다.',
-        },
     )
 
     def validate_proposal_id(self, value):
