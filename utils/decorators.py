07be3e59008e36bb829030f5466ccee605a35944
Hyerin Sung <hyerinx@ewha.ac.kr>
2025-08-22 20:14:17 +0900

    🐛 예외 처리
    - 조건문은 제대로 판별이 되는데, return이 작동하지 않아서 예외 raise로 수정함.

--- utils/decorators.py
+++ utils/decorators.py
@@ -2,6 +2,7 @@ from functools import wraps
 from django.db import IntegrityError
 from rest_framework import status
 from rest_framework.response import Response
+from rest_framework.exceptions import PermissionDenied
 from utils.choices import ProfileChoices
 
 def example(func):
@@ -120,10 +121,7 @@ def require_profile(profile:ProfileChoices):
         @wraps(service_func)
         def wrapper(self, *args, **kwargs):
             if getattr(self.request.user, profile.value, None) is None:
-                return Response(
-                    { "detail": f"{profile.label} 프로필을 생성해 주세요." }, 
-                    status=status.HTTP_400_BAD_REQUEST,
-                )
+                raise PermissionDenied(f"{profile.label} 프로필을 생성해 주세요.")
             return service_func(self, *args, **kwargs)
         return wrapper
     return decorator
