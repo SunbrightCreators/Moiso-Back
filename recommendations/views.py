6d9fe2374670cf7f5bf39ae466a69fdcc5b35b0a
Hyerin Sung <hyerinx@ewha.ac.kr>
2025-08-23 22:54:13 +0900

    🔨 URL 설정, 뷰 보일러플레이트 추가 (#11)

--- recommendations/views.py
+++ recommendations/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.http import HttpRequest
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class ProposalOverall(APIView):
+    def get(self, request:HttpRequest, format=None):
+        return Response(
+            '',
+            status=status.HTTP_200_OK,
+        )
+
+class ProposalScrapSimilarity(APIView):
+    def get(self, request:HttpRequest, format=None):
+        return Response(
+            '',
+            status=status.HTTP_200_OK,
+        )
+
+class ProposalFundingSuccessSimilarity(APIView):
+    def get(self, request:HttpRequest, format=None):
+        return Response(
+            '',
+            status=status.HTTP_200_OK,
+        )
