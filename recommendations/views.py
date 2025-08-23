77edfbf36e77c15c9b0aa2989d3a64ee570f2681
Hyerin Sung <hyerinx@ewha.ac.kr>
2025-08-24 01:25:00 +0900

    🔨 URL 수정 (#11)

--- recommendations/views.py
+++ recommendations/views.py
@@ -3,7 +3,7 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-class ProposalOverall(APIView):
+class ProposalCalc(APIView):
     def get(self, request:HttpRequest, format=None):
         return Response(
             '',
