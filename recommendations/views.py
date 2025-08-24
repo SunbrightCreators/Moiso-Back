dc76b87b9d5dd0286ce1d92381574fcee2b53c11
Hyerin Sung <hyerinx@ewha.ac.kr>
2025-08-24 16:30:27 +0900

    ✨ 스크랩한 제안과 유사한 제안 AI 추천 API 추가 (#11)

--- recommendations/views.py
+++ recommendations/views.py
@@ -2,6 +2,7 @@ from django.http import HttpRequest
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from .services import RecommendationScrapService
 
 class ProposalCalc(APIView):
     def get(self, request:HttpRequest, format=None):
@@ -12,8 +13,11 @@ class ProposalCalc(APIView):
 
 class ProposalScrapSimilarity(APIView):
     def get(self, request:HttpRequest, format=None):
+        service = RecommendationScrapService(request)
+        data = service.recommend_founder_scrap_proposal()
+
         return Response(
-            '',
+            data,
             status=status.HTTP_200_OK,
         )
 
