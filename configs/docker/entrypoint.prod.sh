b875bc43f14f1bca8309ad5a51958b5af2ea3b14
2ewyeonwoo3 <yeonwooo10607@ewha.ac.kr>
2025-09-22 20:30:52 +0900

    nginx, web 설정변경

--- configs/docker/entrypoint.prod.sh
+++ configs/docker/entrypoint.prod.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-# python manage.py collectstatic --no-input echo "Apply database migrations" python manage.py migrate
-python manage.py collectstatic --no-input
-python manage.py makemigrations
-python manage.py migrate
+set -e
+
+# DB 마이그레이션
+python manage.py migrate --noinput
+
+# 정적 파일 수집(반드시 비대화형)
+python manage.py collectstatic --noinput
+
+# 위가 성공하면 CMD 실행(gunicorn은 compose의 command에서 넘김)
 exec "$@"
