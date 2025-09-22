b625d06a5fc9a56c9170a4d58e6a1c53bef5a447
2ewyeonwoo3 <yeonwooo10607@ewha.ac.kr>
2025-09-22 19:03:47 +0900

    🐛STATIC_ROOT가 설정(103)

--- configs/settings/base.py
+++ configs/settings/base.py
@@ -111,13 +111,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.getenv("STATIC_ROOT", "/home/app/web/static")
 
 # Media files
 
 MEDIA_URL = '/media/'
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT  = os.getenv("MEDIA_ROOT",  "/home/app/web/media")
 
 
 # Default primary key field type
