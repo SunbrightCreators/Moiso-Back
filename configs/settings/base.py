e8c9a38eb27cfc641edf3cd629f4ee4581ee5478
2ewyeonwoo3 <yeonwooo10607@ewha.ac.kr>
2025-09-23 20:05:23 +0900

    🐛env파일생성 경로변경(103)

--- configs/settings/base.py
+++ configs/settings/base.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # env
 env = environ.Env(DEBUG=(bool, False))
 
-environ.Env.read_env(os.path.join(BASE_DIR, 'env', '.env.base'))
+environ.Env.read_env(os.path.join(BASE_DIR, 'env', '.env.production'))
 
 SECRET_KEY = env("SECRET_KEY")
 DEBUG = env('DEBUG')
