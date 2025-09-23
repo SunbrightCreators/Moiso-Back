3cb168151e1e70896663810d2c58fe393b845278
2ewyeonwoo3 <yeonwooo10607@ewha.ac.kr>
2025-09-23 17:16:41 +0900

    🐛settings_init_.py 삭제(103)

--- configs/settings/__init__.py
+++ configs/settings/__init__.py
@@ -1,6 +1,2 @@
-from .base import DEBUG
-
-if DEBUG:
-    from .development import *
-else:
-    from .production import *
+# 패키지 초기화 단계에서 __init__.py가 from .base import DEBUG 를 실행하면서 .base를 무조건 임포트해버림. 
+# .env/환경변수 준비 전이라도 SECRET_KEY = env('SECRET_KEY')가 바로 실행되어 실패한다.
