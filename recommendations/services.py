c22632096e13472e1dbc1130aa0d4dc72296ffd6
Hyerin Sung <145767486+hyerindev@users.noreply.github.com>
2025-09-25 22:47:35 +0900

    스크랩 기반 AI 추천 API - NumPy 배열은 truthy/falsy 판단 불가 (#117)
    * 🐛 NumPy 배열은 truthy/falsy 판단 불가
    
    * 💡 주석 수정
    
    * ♻️ + 연산자에서 join으로 개선
    
    * ♻️ 조건 표현식 사용

--- recommendations/services.py
+++ recommendations/services.py
@@ -144,16 +144,11 @@ class RecommendationScrapService:
         valid_vectors = list()
         for post in posts:
             vector = cache.get(cache_key_method(post.id))
-            if not vector:
-                # 각 게시물 벡터 계산
-                vector = self.ai.vectorize(post.title + post.content)
+            if vector is None: # 캐시가 없거나 유효한 벡터가 아닐 때
+                vector = self.ai.vectorize(' '.join([post.title, post.content])) # 게시물 벡터 계산
                 cache.set(cache_key_method(post.id), vector, timeout=365*24*60*60*1) # 수정 불가능하여 데이터가 변경되는 경우가 없으므로 1년 캐싱
-            # 유효한 벡터만 필터링
-            if vector is not None:
-                if option == 'vector':
-                    valid_vectors.append(vector)
-                else:
-                    valid_vectors.append((post, vector))
+            if vector is not None: # 캐시 또는 새로 계산한 값이 유효한 벡터일 때
+                valid_vectors.append(vector if option == 'vector' else (post, vector))
         return valid_vectors
 
     @require_profile(ProfileChoices.founder)
